5277d2ff66a90d3fe4b4f73bd7925ec334235491
mistergad <auditore849@gmail.com>
2018-04-13 19:58:56 +0000

    OpenCV from roscpp

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -118,6 +118,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ubuntu/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/ubuntu/catkin_ws/build/demos/fiducial_follow/cmake_install.cmake")
+  include("/home/ubuntu/catkin_ws/build/opencv_origin/cmake_install.cmake")
 
 endif()
 
